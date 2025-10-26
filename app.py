22fca853137953affd47c4d95f33f9e858af7393
Kaustab Sarkar <kaustabsarkarnabapally@gmail.com>
2025-10-26 17:10:52 +0530

    feat: Add API endpoints to create and list complaints

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
-from flask import Flask
-
-# Create an instance of the Flask class
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-# Define a route for the home page
-@app.route('/')
-def hello_world():
-    return 'Hello, World! My Improve My City backend is running!'
+# This is a temporary in-memory "database"
+# It's just a simple Python list. The data will reset if the server restarts.
+complaints = []
+complaint_id_counter = 1 # To give each complaint a unique ID
 
-# This part is needed to run the app directly
+@app.route('/api/complaints', methods=['GET', 'POST'])
+def handle_complaints():
+    global complaint_id_counter
+
+    if request.method == 'POST':
+        # This block is for receiving a new complaint
+        data = request.get_json()
+        
+        # Create a new complaint dictionary
+        new_complaint = {
+            "id": complaint_id_counter,
+            "title": data.get('title'),
+            "description": data.get('description'),
+            "status": "Pending" # Default status
+        }
+        
+        complaints.append(new_complaint)
+        complaint_id_counter += 1
+        
+        return jsonify({"message": "Complaint successfully submitted!", "complaint": new_complaint}), 201 # 201 means "Created"
+
+    else:
+        # This block is for sending the list of all complaints
+        # The GET method is the default
+        return jsonify(complaints)
+
 if __name__ == '__main__':
     app.run(debug=True)
